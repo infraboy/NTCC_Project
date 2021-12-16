84cce73bb50d198a715f9b59930339254c87a0a2
Ayush Ghosh <ayushkumarghosh@gmail.com>
2021-12-16 11:03:23 +0530

    Added Sign In and Sign Up Activities

--- app/src/main/java/com/example/ntccproject/CreateAccount.java
+++ app/src/main/java/com/example/ntccproject/CreateAccount.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account);
     }
 
-    public void createAccount(View view) {
-        Intent i = new Intent(MainActivity.this, CreateAccount.class);
+    public void signIn(View view) {
+        Intent i = new Intent(CreateAccount.this, MainActivity.class);
         startActivity(i);
         finish();
     }
