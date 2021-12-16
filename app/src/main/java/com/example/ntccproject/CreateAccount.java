4d30e2856be98bf2e574644d7c7cd2e88575a30e
Ayush Ghosh <ayushkumarghosh@gmail.com>
2021-12-16 19:00:40 +0530

    can save account details using shared prefs and login validation

--- app/src/main/java/com/example/ntccproject/CreateAccount.java
+++ app/src/main/java/com/example/ntccproject/CreateAccount.java
@@ -3,19 +3,46 @@ package com.example.ntccproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class CreateAccount extends AppCompatActivity {
+
+    private EditText name, email, username, password, confirmPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirmPass = findViewById(R.id.confirmPass);
     }
 
     public void signUp(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Accounts", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Set<String> accDetails = new HashSet<String>();
+        accDetails.add("password\n" + password.getText().toString());
+        accDetails.add("name\n" + name.getText().toString());
+        accDetails.add("email\n" + email.getText().toString());
+        editor.putStringSet(username.getText().toString(), accDetails);
+        editor.commit();
+        Intent i = new Intent(CreateAccount.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goToSignIn(View view) {
         Intent i = new Intent(CreateAccount.this, MainActivity.class);
         startActivity(i);
         finish();
--- app/src/main/java/com/example/ntccproject/MainActivity.java
+++ app/src/main/java/com/example/ntccproject/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.ntccproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox showPass;
-    private EditText password;
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showPass = findViewById(R.id.showPass);
+        username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         showPass.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -33,5 +41,24 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(MainActivity.this, CreateAccount.class);
         startActivity(i);
         finish();
+    }
+
+    public void signIn(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Accounts", MODE_PRIVATE);
+        Set<String> accDetails = sharedPreferences.getStringSet(username.getText().toString(), new HashSet<String>());
+        if (accDetails.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Username not found!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Map<String, String> details = new HashMap<>();
+        for (String item:accDetails) {
+            String[] items = item.split("\n");
+            details.put(items[0], items[1]);
+        }
+        if (details.get("password").equals(password.getText().toString())) {
+            Toast.makeText(getApplicationContext(),"Logged In", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(),"Password is incorrect!!", Toast.LENGTH_LONG).show();
+        }
     }
 }
